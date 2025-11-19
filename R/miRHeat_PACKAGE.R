af9c0038f6f9c6c76e1470341072a5aaa522ce5c
maria beatriz <maria.nicoletti@unesp.br>
2025-11-19 13:22:15 -0300

    criei o site

---
+++ R/miRHeat_PACKAGE.R
@@ -0,0 +1,10 @@
+#' miRHeat: Ferramentas para análise de interações miRNA–UTR
+#'
+#' O pacote miRHeat fornece funções para ler arquivos de predição de interação
+#' entre genes e miRNAs,
+#' selecionar métricas de score, preparar dados e gerar heatmaps
+#' de interações miRNA × UTR.
+#'
+#' @keywords internal
+"_PACKAGE"
+
